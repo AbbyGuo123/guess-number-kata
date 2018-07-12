add6c2490e5e0414425dd5447fa72a1fa6e1dc46
GUOAB <AbbyGuo@oocl.com>
2018-07-12 19:33:13 +0800

    Unitest Answer validate_inputAnswer function in(1_2_3_3,1_2_10_11,1_2_3_4)

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -3,10 +3,15 @@ package tw.core;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
 import tw.core.model.Record;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -37,6 +42,49 @@ public class AnswerTest {
         //then
         assertThat(record.getValue(),is("2A1B"));
 
+    }
+    @Test
+    public void should_get_the_Record_when_call_validate_input_inputAnswer_1233() throws Exception {
+        //given
+        Answer inputAnswer = new Answer();
+        List<String> numList = Arrays.asList("1","2","3","3");
+        inputAnswer.setNumList(numList);
+        //when
+        try {
+            inputAnswer.validate();
+        }catch (AnswerFormatIncorrectException e){
+            fail("Answer format is incorrect");
+        }
+
+
+    }
+    @Test
+    public void should_get_the_Record_when_call_validate_input_inputAnswer_1_2_10_11() throws Exception {
+        //given
+        Answer inputAnswer = new Answer();
+        List<String> numList = Arrays.asList("1","2","10","11");
+        inputAnswer.setNumList(numList);
+        //when
+        try {
+            inputAnswer.validate();
+        }catch (AnswerFormatIncorrectException e){
+            fail("Answer format is incorrect");
+        }
+
+
+    }
+    @Test
+    public void should_get_the_Record_when_call_validate_input_inputAnswer_1_2_3_4() throws Exception {
+        //given
+        Answer inputAnswer = new Answer();
+        List<String> numList = Arrays.asList("1","2","3","4");
+        inputAnswer.setNumList(numList);
+        //when
+        try {
+            inputAnswer.validate();
+        }catch (AnswerFormatIncorrectException e){
+            fail("Answer format is incorrect");
+        }
     }
 
 }
